abef7863d55ead6ef427dc85142d54bc5c7227ec
Mahmoud Adel <capoking3@gmail.com>
2024-09-14 13:34:31 +0300

    Done Mycourses and conent of courses got from Database

--- enroll.php
+++ enroll.php
@@ -16,6 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
             $stmt->close();
         }
         header('Location: index.php');
+        exit();
     }
 }
 ?>
--- logout.php
+++ logout.php
@@ -2,7 +2,6 @@
 session_start(); // Start the session
 // Destroy the session
 session_destroy();
-
 // Redirect to the login page
 header('Location: login_register.php');
 exit();
