f5456af2dd2cacf1cda4489b394392bb2c62dceb
Mahmoud Adel <capoking3@gmail.com>
2024-09-16 13:46:01 +0300

    Sorting Courses by its Category && Add New Fieald to Courses table on database

--- content.php
+++ content.php
@@ -20,7 +20,17 @@ include('DBconnection.php');
 </div>      
     <!-- content -->
     <?php
-$query = "SELECT * FROM courses";
+$query = "SELECT * FROM courses ORDER BY 
+CASE 
+    WHEN Category = 'Programming' THEN 1
+    WHEN Category = 'Web Development' THEN 2
+    WHEN Category = 'Cybersecurity' THEN 3
+    WHEN Category = 'Design' THEN 4
+    WHEN Category = 'Finance' THEN 5
+    WHEN Category = 'Marketing' THEN 6
+    WHEN Category = 'Blockchain' THEN 7
+    ELSE 8
+END, CourseTitle";
 $result = mysqli_query($conn, $query);
 
 // Initialize $enrolledCourses as an empty array
