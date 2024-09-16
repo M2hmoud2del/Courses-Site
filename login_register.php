b43f4bc7a5a103357347527cffb096a386919a19
Mahmoud Adel <capoking3@gmail.com>
2024-09-16 17:18:56 +0300

    add logo to all sites

--- login_register.php
+++ login_register.php
@@ -7,6 +7,7 @@ session_start(); // Start session to access session variables
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/logo.png" type="image/png">
     <title>Sign In / Register</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
