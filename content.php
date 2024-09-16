bbeb2990ba22d0fa8849df304a2754574da6b5ed
Mahmoud Adel <capoking3@gmail.com>
2024-09-16 10:23:09 +0300

    fix issue in conent

--- content.php
+++ content.php
@@ -28,14 +28,14 @@ echo '<div class="container-fluid mt-5" id="contentContainer">
 
 while($course = mysqli_fetch_assoc($result)) {
     echo '
-        <div class="col-md-4 mb-4" id="column">
+        <div class="col-sm-6 col-md-4 mb-4 " id="column">
             <form action="enroll.php" method="post">
-                <div class="card">
-                    <img class="card-img-top" src="img/' . $course['Image'] . '" alt="' . $course['CourseTitle'] . ' Image" style="height: 180px;">
+                <div class="card " style="width: 450px; height: 450px;">
+                    <img class="card-img-top" src="img/' . $course['Image'] . '" alt="' . $course['CourseTitle'] . ' Image" style="height: 150px; object-fit: cover;">
                     <div class="card-body d-flex flex-column">
                         <div>
-                            <h5 class="card-title">' . $course['CourseTitle'] . '</h5>
-                            <p class="card-text">' . $course['Description'] . '</p>
+                            <h5 class="card-title" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">' . $course['CourseTitle'] . '</h5>
+                            <p class="card-text" style="height: 60px; overflow: hidden;">' . $course['Description'] . '</p>
                             <p class="card-text">Instructor: ' . $course['Instructor'] . '</p>
                             <p class="card-text">Price: $' . $course['Price'] . '</p>
                             <input type="hidden" name="course_id" value="' . $course['Course_ID'] . '">
@@ -51,6 +51,4 @@ while($course = mysqli_fetch_assoc($result)) {
 
 echo '  </div>
       </div>';
-
-
 ?>
